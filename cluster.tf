72c7191ffb56c65c4eabb9a926d528d11a370a39
Damola Aderinwale <damolaaderinwale@Damolas-MacBook-Pro.local>
2024-12-03 00:21:11 -0500

    feat: added BYO cluster functionality to docs

--- cluster.tf
+++ cluster.tf
@@ -7,11 +7,11 @@ resource "aws_ecs_cluster" "cluster" {
 }
 
 
-data "aws_ecs_cluster" "codepipeline_bucket" {
+data "aws_ecs_cluster" "cluster_derived" {
   count  = var.use_default_cluster ?  0 : 1
   cluster_name = var.project_name
 }
 
 locals {
-  aws_ecs_cluster = var.use_default_cluster ? aws_ecs_cluster.codepipeline_bucket : data.aws_ecs_cluster.codepipeline_bucket
+  aws_ecs_cluster = var.use_default_cluster ? aws_ecs_cluster.cluster : data.aws_ecs_cluster.cluster_derived
 }
